7f688811f46351e43bfc51cdc9944f57e892dd56
Charles Moulliard <cmoulliard@redhat.com>
2019-08-29 09:51:57 +0200

    Update doc

--- scripts/it.sh
+++ scripts/it.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 #
-#  ./it.sh agnosticd/ansible/roles ANSIBLE_DIR ACTION GUID ENV
+#  ./scripts/it.sh agnosticd/ansible/roles ANSIBLE_DIR ACTION GUID ENV
 
 ANSIBLE_DIR=$1
 ACTION=${2:-create}
-GUID=${3:-namur-7027}
+GUID=${3:-rrrrr-xxxx}
 ENV=${4:-prod}
 OCP_WORKLOAD="ocp-workload-dekorate-component-operator"
 SSH_PRIVATE_KEY="id_rsa"
@@ -13,15 +13,17 @@ SSH_PRIVATE_KEY="id_rsa"
 # Prod environment
 # RHPDS
 # OCP_USERNAME="cmoulliard-redhat.com"
-# HOST_GUID="namur-dfb4"
+# HOST_GUID="rrrrr-xxxx"
 # GUID=$HOST_GUID
-# TARGET_HOST="bastion.$HOST_GUID.openshiftworkshop.com"
+# TARGET_HOST="bastion.$HOST_GUID.open.redhat.com"
 prod () {
   echo "=== PROD env ===="
   OCP_USERNAME="cmoulliard-redhat.com"
   HOST_GUID=${GUID}
   GUID=$HOST_GUID
   TARGET_HOST="bastion.$HOST_GUID.open.redhat.com"
+  echo "=== Bastion : ${TARGET_HOST}"
+  echo "=== OCP user : ${OCP_USERNAME}"
 }
 
 # Development environment
@@ -36,6 +38,8 @@ dev () {
   # OCP_USERNAME="opentlc-mgr"
   GUID="snowdroptest"
   TARGET_HOST=bastion.dev.openshift.opentlc.com
+  echo "=== Bastion : ${TARGET_HOST}"
+  echo "=== OCP user : ${OCP_USERNAME}"
 }
 
 echo "Provision the environment to be used"
@@ -48,14 +52,15 @@ fi
 # Define the user to be used to ssh
 SSH_USER=$OCP_USERNAME
 
-# ansible-playbook -i $TARGET_HOST, ./scripts/ocp-workload.yml \
-#     -e "ansible_roles_path=$ANSIBLE_DIR" \
-#     -e "ansible_ssh_private_key_file=~/.ssh/${SSH_PRIVATE_KEY}" \
-#     -e "ansible_user=${OCP_USERNAME}" \
-#     -e "ocp_username=${OCP_USERNAME}" \
-#     -e "ocp_workload=${OCP_WORKLOAD}" \
-#     -e "silent=False" \
-#     -e "guid=${GUID}" \
-#     -e "ACTION=$ACTION" \
-#     -v
+echo #Execute ansible playbook - workshop for lab 2026"
+ansible-playbook -i $TARGET_HOST, ./scripts/ocp-workload.yml \
+    -e "ansible_roles_path=$ANSIBLE_DIR" \
+    -e "ansible_ssh_private_key_file=~/.ssh/${SSH_PRIVATE_KEY}" \
+    -e "ansible_user=${OCP_USERNAME}" \
+    -e "ocp_username=${OCP_USERNAME}" \
+    -e "ocp_workload=${OCP_WORKLOAD}" \
+    -e "silent=False" \
+    -e "guid=${GUID}" \
+    -e "ACTION=$ACTION" \
+    -v
 
