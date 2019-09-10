a4e63f6bc3bb889b96565e2e32b435de145cee68
Charles Moulliard <cmoulliard@redhat.com>
2019-09-10 18:45:06 +0200

    docs: Add bash command to call the bash script able to create the git repo

--- git-create.sh
+++ git-create.sh
@@ -18,4 +18,4 @@ token="!demo12345"
 echo -e "Creating Github repository '$repo_name' under '$org_name' ..."
 echo "curl -u "xxxxx:yyyyy" https://api.github.com/orgs/$org_name/repos -d '{"name":"'$repo_name'", "description":"My cool '$repo_name'", "private": false, "has_issues": false, "has_projects": true, "has_wiki":false }'"
 curl -u "$username:$token" https://api.github.com/orgs/$org_name/repos -d '{"name":"'$repo_name'", "description":"My cool '$repo_name'", "private": false, "has_issues": false, "has_projects": true, "has_wiki":false }' > /dev/null 2>&1
-echo " done."
+echo "done."
